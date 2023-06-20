c566debfe7809435cd7be20dd6ed98845bdff177
ShreyasK06 <92545590+ShreyasK06@users.noreply.github.com>
2023-06-20 14:57:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const quoteContainer = document.getElementById("quote-container");
+const quotetext = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("whatsapp");
+const newQuoteBtn = document.getElementById("new-quote");
+let apiQuotes = [];
+
+// show new quote
+function newQuote() {
+    const randQuote = apiQuotes[Math.round(Math.random() * apiQuotes.length)];
+    if (!randQuote.author) {
+        authorText.innerText = "UNKNOWN";
+    } else {
+        authorText.innerText = randQuote.author;
+    }
+
+    quotetext.innerText = randQuote.text;
+
+}
+
+
+// Get Quote From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log("whoops, no quote", error);
+    }
+
+}
+
+// On Load
+getQuotes();
+newQuoteBtn.addEventListener('click', newQuote);
